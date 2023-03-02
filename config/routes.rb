b3d497fac42a7b0d047c9c451b3ff36f80121060
Shailendra1703 <singhshailendra1701@gmail.com>
2023-03-03 00:20:04 +0530

    Implemented some basic functionality in rails for blog website

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "articles#index"
-  get "/articles", to: "articles#index"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  resources :articles
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_30_110724) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_130511) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "students", force: :cascade do |t|
     t.string "firstName"
     t.string "lastName"
